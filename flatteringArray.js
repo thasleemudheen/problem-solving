c4bb32ac43a2062b2ef7ee52d2eadf398183b3ae
thasleem <thasleemtrk@gmail.com>
2024-08-23 10:55:20 +0530

    flatarray

--- flatteringArray.js
+++ flatteringArray.js
@@ -1,4 +1,22 @@
 let inputArr=[1, [2, [3, 4], 5], 6]
 
-let newArr=inputArr.flat(Infinity)
-console.log(newArr)
+
+// let newArr=inputArr.flat(Infinity)
+// console.log(newArr)
+
+
+
+function flatArr(inputArr){
+    let result=[]
+    for(let i=0;i<inputArr.length;i++){
+        if(typeof(inputArr[i])==='number'){
+                 result.push(inputArr[i])
+        }else if(Array.isArray(inputArr[i])) {
+           result=result.concat(flatArr(inputArr[i]))
+        }
+        
+    }    
+    return result
+}
+
+console.log(flatArr(inputArr));
