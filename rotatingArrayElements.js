bb074a19b949c65d2db6fffb940b5a36dc2d30b5
thasleem <thasleemtrk@gmail.com>
2024-09-18 17:57:56 +0530

    rotate array elements

--- rotatingArrayElements.js
+++ rotatingArrayElements.js
@@ -1,11 +1,35 @@
+
+// function rotateArr(array,k){
+//     const size=array.length
+//     k=k%size
+//     const rotatedPart=array.splice(-k)
+//     array.unshift(...rotatedPart)
+//     return array
+// }
+
+// console.log(rotateArr(array,k))
+
 const array=[1,2,3,4,5,6,7,8,9]
 let k=4;
 function rotateArr(array,k){
     const size=array.length
-    k=k%size
-    const rotatedPart=array.splice(-k)
-    array.unshift(...rotatedPart)
+    if(size<k){
+        k=k%size
+    }
+    reverse(array,0,array.length-1)
+    reverse(array,0,k-1)
+    reverse(array,k,array.length-1)
     return array
 }
 
+function reverse(array,left,right){
+    while (left<right) {
+        let temp=array[left]
+        array[left]=array[right]
+        array[right]=temp
+        left++;
+        right--;
+    }
+    return array
+}
 console.log(rotateArr(array,k))
