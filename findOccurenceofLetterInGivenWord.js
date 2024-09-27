880fc9596430e5d85120ccc53f540865d68f72b3
thasleem <thasleemtrk@gmail.com>
2024-09-27 09:44:19 +0530

    find occurence of letter in given word

---
+++ findOccurenceofLetterInGivenWord.js
@@ -0,0 +1,14 @@
+const str="javascript is awesome"
+const letter='a'
+
+function findOccurence(str,letter){
+    let count=0;
+    for(let i=0;i<str.length;i++){
+        if(str[i]===letter){
+            count++;
+        }
+    }
+    return count
+}
+
+console.log(findOccurence(str,letter))
